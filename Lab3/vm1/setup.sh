b944b8a3b7625a710a25cf522f02bfa6af9a6237
furaizi <furachannel16@gmail.com>
2025-05-10 01:07:58 +0300

    Add email sending feature to VM1

--- Lab3/vm1/setup.sh
+++ Lab3/vm1/setup.sh
@@ -6,23 +6,20 @@ named-checkconf
 named-checkzone planet.edu /etc/bind/db.planet.edu
 named-checkzone zone02.org /etc/bind/db.zone02.org
 named-checkzone 11.168.192.in-addr.arpa /etc/bind/db.192.168.11
-named -u bind -g
+named -u bind -g &
 
 update-ca-certificates
-service rsyslog start
+# Initialize services
 service postfix start
-
-# Set up Dovecot
-sed -i 's/^#*\s*disable_plaintext_auth.*/disable_plaintext_auth = yes/' \
-    /etc/dovecot/conf.d/10-auth.conf
-sed -i 's/^#*\s*auth_mechanisms.*/auth_mechanisms = plain login/' \
-    /etc/dovecot/conf.d/10-auth.conf
-cat > /etc/dovecot/conf.d/10-ssl.conf <<EOF
-ssl = required
-ssl_cert = </etc/ssl/local/${HOSTNAME}.crt
-ssl_key  = </etc/ssl/local/${HOSTNAME}.key
-ssl_ca   = </etc/ssl/local/planet-ca.crt
-ssl_min_protocol = TLSv1.2
-EOF
-
 service dovecot start
+
+# Create mail addresses
+postmap /etc/postfix/vmailbox
+for u in mercury venus earth; do
+  mkdir -p /var/mail/vhosts/planet.edu/$u/Maildir
+done
+groupadd --system --gid 5000 vmail
+useradd  --system --uid 200 --gid vmail \
+         --home-dir /var/mail/vhosts --shell /usr/sbin/nologin vmail
+chown -R vmail:vmail /var/mail/vhosts
+chmod -R 700 /var/mail/vhosts
