76d98cc28b62f9d969415a33a199fc70c9241f1c
furaizi <furachannel16@gmail.com>
2025-05-11 00:41:30 +0300

    Add TLS and IMAPS connection feature for mail server

--- Lab3/vm1/setup.sh
+++ Lab3/vm1/setup.sh
@@ -16,10 +16,12 @@ service dovecot start
 # Create mail addresses
 postmap /etc/postfix/vmailbox
 for u in mercury venus earth; do
-  mkdir -p /var/mail/vhosts/planet.edu/$u/Maildir
+  for f in cur new tmp; do
+    mkdir -p /var/mail/vhosts/planet.edu/$u/Maildir/$f
+  done
 done
 groupadd --system --gid 5000 vmail
-useradd  --system --uid 200 --gid vmail \
+useradd  --system --uid 5000 --gid vmail \
          --home-dir /var/mail/vhosts --shell /usr/sbin/nologin vmail
 chown -R vmail:vmail /var/mail/vhosts
 chmod -R 700 /var/mail/vhosts
