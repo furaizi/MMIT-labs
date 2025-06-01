b99fad3334432edd553b7dbf0fa4d50d690a2cf8
furaizi <furachannel16@gmail.com>
2025-06-01 14:31:46 +0300

    Make entrypoint.sh script idempotent in Lab6

--- Lab6/vm1/entrypoint.sh
+++ Lab6/vm1/entrypoint.sh
@@ -3,17 +3,23 @@
 set -e
 
 for u in mercury venus saturn guest; do
-    adduser --disabled-password --gecos "" "$u"
+    # Linux users
+    adduser --disabled-password --gecos "" "$u" || true
     echo "$u:pass" | chpasswd
+
+    # Samba users
     if [ "$u" = "guest" ]; then
-        smbpasswd -a -s "$u"
+        smbpasswd -a -n "$u" || true
     else
-        printf "pass\npass\n" | smbpasswd -a -s "$u"
+        printf "pass\npass\n" | smbpasswd -a -s "$u" || true
     fi
-    smbpasswd -e "$u"
+    smbpasswd -e "$u" || true
+
+    # Create home directories
+    mkdir -p /srv/samba/home/"$u"
 done
 
-for d in hidden home public incoming; do
+for d in hidden public incoming; do
     mkdir -p /srv/samba/"$d"
 done;
 
